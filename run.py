ff4196fa7a047abd8257983d0bd6c25235ff7e64
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-23 22:58:11 +0300

    create short code for login detail

--- run.py
+++ run.py
@@ -52,11 +52,69 @@ def find_credentials(names):
     function to find credentials by account name
     '''
     return Credentials.find_by_name(names)
+
 def display_credentials():
     '''
     functions that returns all the saved credentials
     '''
     return Credentials.display_credentials()
+
+def main():
+    print("Hello There, Welcome to Password Locker...")
+    print("Please find navigation around the locker below")
+    print("\n")
+    print("lg => to login to your account")
+    print("sn => to Sign-Up and create account.")
+    short_code = input().lower()
+    print("\n")
+    if short_code == "sn":
+                        print("Enter UserName")
+                        username = input()
+                        
+                        print("Enter Password")
+                        password = input()
+                        
+                        print("Confirm Password")
+                        confirm_password = input()
+                        
+                        while confirm_password != password:
+                            print("Try again! passwords does not match!")
+                            print("Enter password")
+                            password = input()
+                            print("Confirm Password")
+                            confirm_password = input()
+                            
+                        else:
+                            print(f"Hi {username}, Your account has been successfully created!")
+                            print("\n")
+                            print("Login to your account")
+                            print("Enter your Username")
+                            username_input = input()
+                            print("Entre your Password")
+                            password_input = input()
+                            
+                        while username_input != username or password_input != password:
+                            print("Invalid username or password")
+                            
+                            print("Enter your Username")
+                            username_input = input()
+                            print("Enter your Password")
+                            password_input = input()
+                            
+                        else:
+                            print(f"Hi {username}, Welcome to Password LOcker")
+                            while True:
+                              print("Choose an option to continue")
+                              print("nc => to create new credentials")
+                              print("dc => to display credentials")
+                              print("cc => to copy credentials to clipboard")
+                              print("da => to delete account credentias")
+                              print("ex => to exit password locker")
+                              
+                              short_code = input().lower()
+                              print("\n")
+                              
+                            
         
 
         
