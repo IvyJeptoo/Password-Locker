04f8f841bc0b8a5ca8f37ee04d055cf852c7c4da
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-23 17:10:02 +0300

    create new creadentials function

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
+from hashlib import new
 import pyperclip
+from credentials import Credentials
 from userInfo import User
 
-def create_def(username, password):
+def create_user(username, password):
     '''
     creates a new user
     '''
     new_user = User(username, password)
     return new_user
+
+def create_credentials(accountname, login):
+    '''
+    function to create new credential
+    '''
+    new_credentials = Credentials(accountname, login)
+    return new_credentials
