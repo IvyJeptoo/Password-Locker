9db4c39495ee8eca27d1556f940e13133ac676ee
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-23 20:29:50 +0300

    create function to save, delete,find and display credentials

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from hashlib import new
-import pyperclip
+import pyperclip, random, string
 from credentials import Credentials
 from userInfo import User
 
@@ -10,9 +9,56 @@ def create_user(username, password):
     new_user = User(username, password)
     return new_user
 
+
+def generate_login():
+    '''
+    function that generate a login
+    '''
+    phrase = string.digits + string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        try:
+            length = int(input("Enter desired length of login "))
+            login = random.sample(phrase,length)
+            
+        except ValueError:
+            print("Enter a valid number please!")
+            continue
+        else:
+            login = ("".join(login))
+            return login
+        
 def create_credentials(accountname, login):
     '''
     function to create new credential
     '''
     new_credentials = Credentials(accountname, login)
     return new_credentials
+       
+
+def save_credentials(credentials):
+    '''
+    function to save credentials
+    '''   
+    credentials.save_credentials()
+    
+def delete_credentials(credentials):
+    '''
+    function to delete credentials
+    '''
+    credentials.delete_credentials()
+    
+def find_credentials(names):
+    '''
+    function to find credentials by account name
+    '''
+    return Credentials.find_by_name(names)
+def display_credentials():
+    '''
+    functions that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
+        
+
+        
+        
+    
