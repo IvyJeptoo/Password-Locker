872fd446f0ae21d695b0cb7ed4172481a40d19d0
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-23 15:58:32 +0300

    create teardown that cleans test

--- userInfo_test.py
+++ userInfo_test.py
@@ -15,3 +15,30 @@ class TestUser(unittest.TestCase):
         '''
         
         self.new_user = User("Stacy "," Stacy2009") #Creates new user
+        
+    def test_init(self):
+        '''
+        tests if the object has been initialized properly
+        '''
+        
+        self.assertEqual(self.new_user.username,"Stacy")
+        self.assertEqual(self.new_user.password,"Stacy2009")
+        
+    def test_save_user(self):
+        '''
+        test case to test if the user object is being saved to the userlist
+        '''
+        #saving new user
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+            
+            
+    def tearDown(self):
+           '''
+           Cleans up after each test running
+           '''
+           User.user_list = []
+           
+if __name__ == '__main__':
+    unittest.main()
+        
