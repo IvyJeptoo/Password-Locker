f4b397bc3d875a05e45ee148aca230a6d40a761d
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-23 16:24:59 +0300

    add function to create user

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 import pyperclip
 from userInfo import User
 
+def create_def(username, password):
+    '''
+    creates a new user
+    '''
+    new_user = User(username, password)
+    return new_user
